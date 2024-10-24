7057cce5979b1c77149d0178bfa131702eb1dce9
MateuszZalocha <162693735+MateuszZalocha@users.noreply.github.com>
2024-10-24 14:58:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Laboratorium1
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Podaj liczbę");
+    //        var input = Console.ReadLine();
+
+    //        Console.WriteLine(IsEven(int.Parse(input)) ? "Parzysta" : "Nieparzysta");
+
+    //    }
+
+
+    //    static bool IsEven(int input)
+    //    {
+    //        return input % 2 == 0;
+    //    }
+
+
+    //    }
+    //}
+    class Program
+    {
+        static void Main()
+        {
+            int zakrespoczatkowy = 1;
+            int zakreskoncowy = 30;
+
+            Console.WriteLine("Liczby Parzyste miedzy {0} a {1} to", zakrespoczatkowy, zakreskoncowy);
+
+
+            for (int i = zakrespoczatkowy; i <= zakreskoncowy; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
