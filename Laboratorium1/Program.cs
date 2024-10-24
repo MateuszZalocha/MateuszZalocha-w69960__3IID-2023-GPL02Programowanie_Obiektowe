df85d790cee47e8fd7da1e5711d99ebb2f81d160
w69960 <w69960@RA117B-15>
2024-10-24 14:05:33 +0200

    Dodaj pliki projektów.

---
+++ Laboratorium1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratorium1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
