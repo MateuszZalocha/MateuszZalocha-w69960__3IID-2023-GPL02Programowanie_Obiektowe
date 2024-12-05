1d4917df018b1142322b5d681492a9f9c1ec9670
Mateusz Załocha <w69960@student.wsiz.edu.pl>
2024-12-05 14:44:49 +0100

    innit

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -27,6 +27,7 @@ public class Square : Shape
 {
     public double SideLength { get; set; }
 
+    
     public Square(double sideLength)
     {
         SideLength = sideLength;
@@ -36,6 +37,87 @@ public class Square : Shape
     public override double CalculateArea()
     {
         return SideLength * SideLength; 
+    }
+}
+
+public interface IVehicle
+{
+    
+    void Start();
+
+    
+    void Stop();
+
+    
+    int MaxSpeed { get; }
+}
+
+public class Car : IVehicle
+{
+    public int MaxSpeed { get; private set; }
+    public string Brand { get; set; }
+
+    
+    public Car(int maxSpeed, string brand)
+    {
+        MaxSpeed = maxSpeed;
+        Brand = brand;
+    }
+
+    
+    public void Start()
+    {
+        Console.WriteLine("Samochód uruchomiony.");
+    }
+
+    
+    public void Stop()
+    {
+        Console.WriteLine("Samochód zatrzymany.");
+    }
+
+    
+    public void Honk()
+    {
+        Console.WriteLine("Samochód trąbi!");
+    }
+}
+
+public class Bike : IVehicle
+{
+    public int MaxSpeed { get; private set; }
+    public bool HasBell { get; set; }
+
+    
+    public Bike(int maxSpeed, bool hasBell)
+    {
+        MaxSpeed = maxSpeed;
+        HasBell = hasBell;
+    }
+
+    
+    public void Start()
+    {
+        Console.WriteLine("Rower ruszył.");
+    }
+
+    
+    public void Stop()
+    {
+        Console.WriteLine("Rower zatrzymany.");
+    }
+
+    
+    public void RingBell()
+    {
+        if (HasBell)
+        {
+            Console.WriteLine("Dzwonek rowerowy dzwoni!");
+        }
+        else
+        {
+            Console.WriteLine("Rower nie ma dzwonka.");
+        }
     }
 }
 
@@ -50,5 +132,20 @@ class Program
         
         Console.WriteLine($"Pole koła: {circle.CalculateArea()}");
         Console.WriteLine($"Pole kwadratu: {square.CalculateArea()}");
+
+        
+        IVehicle car = new Car(200, "Toyota");
+        IVehicle bike = new Bike(30, true);
+
+        
+        car.Start();
+        Console.WriteLine($"Maksymalna prędkość samochodu: {car.MaxSpeed} km/h");
+        ((Car)car).Honk(); 
+
+        bike.Start();
+        Console.WriteLine($"Maksymalna prędkość roweru: {bike.MaxSpeed} km/h");
+        ((Bike)bike).RingBell(); 
     }
 }
+
+
