6ec3c83ead03f3a689c60f21f4111e67610e9879
Mateusz Załocha <w69960@student.wsiz.edu.pl>
2024-12-05 14:05:19 +0100

    innit

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public abstract class Shape
+{
+    
+    public abstract double CalculateArea();
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius; 
+    }
+}
+
+public class Square : Shape
+{
+    public double SideLength { get; set; }
+
+    public Square(double sideLength)
+    {
+        SideLength = sideLength;
+    }
+
+    
+    public override double CalculateArea()
+    {
+        return SideLength * SideLength; 
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Shape circle = new Circle(5); 
+        Shape square = new Square(4); 
+
+        
+        Console.WriteLine($"Pole koła: {circle.CalculateArea()}");
+        Console.WriteLine($"Pole kwadratu: {square.CalculateArea()}");
+    }
+}
