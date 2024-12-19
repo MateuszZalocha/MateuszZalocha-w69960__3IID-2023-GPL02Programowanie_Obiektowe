420b1b2a5a569675ce0ff3816f6aa23316c4db2e
Mateusz Załocha <w69960@student.wsiz.edu.pl>
2024-12-19 14:41:21 +0100

    innit

---
+++ WebAppli/Controllers/lab7Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppli.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class lab7Controller : ControllerBase
+    {
+        private readonly IBookRepository repository;
+
+        public lab7Controller()
+        {
+            repository = new BookRepository();
+        }
+
+        [HttpGet]
+        public List<Book> GetAllBooks()
+        {
+            return repository.GetAllBooks();
+        }
+
+        public IBookRepository GetRepository()
+        {
+            return repository;
+        }
+
+        [HttpPost]
+        public bool AddBook(Book book, IBookRepository repository)
+        {
+            Add(book);
+            return true;
+        }
+
+        private void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAppli/Controllers/labpersonController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppli.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class labpersonController : ControllerBase
+    {
+        private readonly IPersonRepository repository;
+
+        public labpersonController()
+        {
+            repository = new PersonRepository();
+        }
+
+        [HttpGet]
+        public List<Person> GetAllPersons()
+        {
+            return repository.GetAllPersons();
+        }
+
+        public IPersonRepository GetRepository()
+        {
+            return repository;
+        }
+
+        [HttpPost]
+        public bool AddBookToPersonBorrowedBooks(Person person, Book book)
+        {
+            repository.AddBookToPersonBorrowedBooks(person, book);
+            return true;
+        }
+    }
+}
+
